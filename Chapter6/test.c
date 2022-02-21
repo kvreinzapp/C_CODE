965adb354370f063452babb02150a45652d5d867
klaus <18848113328@163.com>
2022-02-21 19:21:39 +0800

    almost

--- Chapter6/test.c
+++ Chapter6/test.c
@@ -1,23 +1,44 @@
 /*
-第六章：编程练习 7
+第六章：编程练习 12
 */
 #include <stdio.h>
-#include <string.h>
 int main(void)
 {
-    char word[30];
-    printf("Please enter the words: ");
-    scanf("%s", word);
-    /* scanf()函数读取用户输入，保存至word字符数组。*/
-    printf("The word you enter is : %s\n", word);
-    printf("The reverse word you enter is : ");
-    
-    for (int i = strlen(word) - 1; i >= 0; i--)
+    int length;
+    double sum = 0.0;
+    printf("Enter the limit length: ");
+    scanf("%d", &length);
+    while (length > 0)
     {
-        printf("%c", word[i]);
+        sum = 0.0;
+        for (int i = 1; i <= length; i++)
+        {
+            sum = sum + 1.0 / i;
+        }
+        /* 计算 1 + 1/2 + 1/3 …… */
+        printf("The sum for 1.0 +...+ 1.0/%d.0 is %lf\n", length, sum);
+        sum = 0.0;
+        for (int i = 1; i <= length; i++)
+        {
+            if (i % 2 == 0)
+                sum = sum - 1.0 / i;
+            else
+                sum = sum + 1.0 / i;
+        }
+        /* 计算 1 - 1/2 + 1/3 - 1/4 ……*/
+        printf("The sum for 1.0 -...+ 1.0/%d.0 is %lf\n", length, sum);
+
+        sum = 0.0;
+        for (int i = 1; i <= length; i++)
+        {
+            if (i % 2 != 0)
+                sum = sum + 2 * 1.0 / i;
+        } /* 两个数列和，只计算奇数项，偶数项相抵消。*/
+        printf("The sum for 1.0 + 1.0+ 2.0/3.0+...+ 2.0/%d.0 is %lf\n", length, sum);
+
+        printf("Enter the limit length: ");
+        scanf("%d", &length);
     }
-    printf("\n");
-    /* 逆序打印，下标值通过strlen()函数获得。strlen()返回的长度值
-     * 不包含'\0',但是数组下标从0～(长度-1)*/
+    printf("\nDone!\n");
     return 0;
 }
