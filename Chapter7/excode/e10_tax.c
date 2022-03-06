d459d8cddedb221c9b84b0be3e7fff1d8e9678f3
klaus <18848113328@163.com>
2022-03-06 11:25:59 +0800

    ex10 2hrs

--- Chapter7/excode/e10_tax.c
+++ Chapter7/excode/e10_tax.c
@@ -12,52 +12,68 @@ For example, a single wage earner with a taxable income of $20,000 owes 0.15 × 
 and the taxable income and that then calculates the tax. Use a loop so that the user can
 enter several tax cases.
 */
-#include<stdio.h>
+#include <stdio.h>
 #define FIR_RATE 0.15
 #define SEC_RATE 0.28
 
-#define SINGLE   17850
-#define HEAD     23900
-#define M_JOINT  29750
-#define M_SEPA   14875
+#define SINGLE 17850
+#define HEAD 23900
+#define M_JOINT 29750
+#define M_SEPA 14875
 
 void show_menu(void);
+void cal_dis(int input, double limit);
 
 int main(void)
 {
-    //Part0: hint
-    show_menu();
-    //Part1:
-    int input;
-    double money;
-    printf("Enter your choice here: ");
-
-    switch (input)
+    while (1)
     {
-    case 1:
-        money=SINGLE;
-        break;
-    case 2:
-        money = HEAD;
-        break;
-    case 3:
-        money = M_JOINT;
-        break;
-    case 4:
-        money = M_SEPA;
-        break;
-    case 5:
-        break;
-    default:
-        break;
+        // Part1: hint
+        show_menu();
+
+        // Part2: choose
+        int input;
+        double limit;
+        printf("Enter your choice here: ");
+        scanf("%d", &input);
+        
+        switch (input)
+        {
+        case 1:
+            limit = SINGLE;
+            break;
+        case 2:
+            limit = HEAD;
+            break;
+        case 3:
+            limit = M_JOINT;
+            break;
+        case 4:
+            limit = M_SEPA;
+            break;
+        case 5:
+            break;
+        default:
+            printf("Please try again, enter integer from 1~5.\nTry it again!\n\n");
+            continue;
+        }
+        if (input==5)
+        {
+            printf("Bye!\n");
+            break;
+        }
+            
+        printf("You chosed %d).\n", input);
+        printf("Your tax strategy is:\" 15%% of first $%.f plus 28%% of excess.\"\n\n", limit);
+
+        // Part3:calculate and display
+        cal_dis(input, limit);
     }
-    printf("You chosed %d), and your tax strategy is 15%% of first $%f plus 28%% of excess"
-            ,input,money);
+    
     
 
-        return 0;
+    return 0;
 }
-
 
 // Part-1: show menu
 void show_menu(void)
@@ -71,4 +87,19 @@ void show_menu(void)
     for (int i = 0; i < 48; i++)
         printf("*");
     printf("\n");
+}
+//Part-2: choose your identity
+
+
+// Part-3: calculate and display
+void cal_dis(int input, double limit)
+{
+    double tax, money;
+    printf("Enter your income here: ");
+    scanf("%lf", &money);
+
+    tax = limit * FIR_RATE + ((money - limit) * SEC_RATE);
+    printf("Your income is $%.2f, your tax is $%.2f, you earned $%.2f.\n"
+            ,money,tax,(money-tax));
+    printf("Continue...\n\n");
 }
