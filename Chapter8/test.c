6e661d081328e15f5aa34d72ce4b8c64bdb612ab
klaus <18848113328@163.com>
2022-03-11 23:24:05 +0800

    ch8

---
+++ Chapter8/test.c
@@ -0,0 +1,20 @@
+ /* echo_eof.c -- repeats input to end of file */
+  #include <stdio.h>
+
+  int main(void)
+
+  {
+
+      int ch;
+
+  
+
+      while ((ch = getchar()) != EOF)
+
+          putchar(ch);
+
+  
+
+      return 0;
+
+  }  
