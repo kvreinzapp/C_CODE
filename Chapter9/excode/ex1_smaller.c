9fcbb42351f63e3ca14b4cf9aa84c2d9731904c1
klaus <18848113328@163.com>
2022-04-04 23:17:48 +0800

    10 total

---
+++ Chapter9/excode/ex1_smaller.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+double min(double, double);
+int main(void)
+{
+    double a, b;
+    printf("Enter two number:");
+    while (2 == scanf("%lf %lf", &a, &b))
+    {
+        printf("The smaller one is %f\n", min(a, b));
+        printf("Please ")
+    }
+    
+    
+
+
+    return 0;
+}
+double min(double x, double y)
+{
+    return x < y ? x : y;
+}
