a879de8616a84b5e833784e201e2ce95ec422a76
klaus <18848113328@163.com>
2022-05-02 19:10:49 +0800

    4 to be done

---
+++ Chapter10/excode/e11_double_prt.c
@@ -0,0 +1,10 @@
+/*Write a program that declares a 3×5 array of  int and initializes it to some values of
+your choice. Have the program print the values, double all the values, and then display
+the new values. Write a function to do the displaying and a second function to do the
+doubling. Have the functions take the array name and the number of rows as arguments.
+*/
+#include <stdio.h>
+#include "Chapter10.h"
+int main(void)
+{
+}
