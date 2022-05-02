bb989cc57aaaff68b94e68216c8964bb7492a97e
klaus <18848113328@163.com>
2022-05-02 19:19:59 +0800

    cant understand now

--- Chapter10/bookcode/10_18_vararr2d.c
+++ Chapter10/bookcode/10_18_vararr2d.c
@@ -4,50 +4,20 @@
 #define COLS 4
 
 int sum2d(int rows, int cols, int ar[rows][cols]);
-<<<<<<< HEAD
 int main(void)
 {
-    int i, j;
-    int rs = 3;
-    int cs = 10;
-
-    int junk[ROWS][COLS] = {
-        {2, 4, 6, 8},   
-        {3, 5, 7, 9},
-        {12, 10, 8, 6}};
-
-    int morejunk[ROWS - 1][COLS + 2] = {
-        {20, 30, 40, 50, 60, 70},
-        {5, 6, 7, 8, 9, 10}};
-
-    int varr[rs][cs]; // VLA
-    for (i = 0; i < rs; i++)
-        for (j = 0; j < cs; j++)
-            varr[i][j] = i * j + j;
-
-    printf("3x5 array\n");
-    printf("Sum of all elements = %d\n",
-           sum2d(ROWS, COLS, junk));
-
-    printf("2x6 array\n");
-    printf("Sum of all elements = %d\n",
-           sum2d(ROWS - 1, COLS + 2, morejunk));
-
-    printf("3x10 VLA\n");
-    printf("Sum of all elements = %d\n",
-           sum2d(rs, cs, varr));
-
-    return 0;
-=======
-int main(void) {
   int i, j;
   int rs = 3;
   int cs = 10;
 
-  int junk[ROWS][COLS] = {{2, 4, 6, 8}, {3, 5, 7, 9}, {12, 10, 8, 6}};
+  int junk[ROWS][COLS] = {
+      {2, 4, 6, 8},
+      {3, 5, 7, 9},
+      {12, 10, 8, 6}};
 
-  int morejunk[ROWS - 1][COLS + 2] = {{20, 30, 40, 50, 60, 70},
-                                      {5, 6, 7, 8, 9, 10}};
+  int morejunk[ROWS - 1][COLS + 2] = {
+      {20, 30, 40, 50, 60, 70},
+      {5, 6, 7, 8, 9, 10}};
 
   int varr[rs][cs]; // VLA
   for (i = 0; i < rs; i++)
@@ -55,21 +25,24 @@ int main(void) {
       varr[i][j] = i * j + j;
 
   printf("3x5 array\n");
-  printf("Sum of all elements = %d\n", sum2d(ROWS, COLS, junk));
+  printf("Sum of all elements = %d\n",
+         sum2d(ROWS, COLS, junk));
 
   printf("2x6 array\n");
-  printf("Sum of all elements = %d\n", sum2d(ROWS - 1, COLS + 2, morejunk));
+  printf("Sum of all elements = %d\n",
+         sum2d(ROWS - 1, COLS + 2, morejunk));
 
   printf("3x10 VLA\n");
-  printf("Sum of all elements = %d\n", sum2d(rs, cs, varr));
+  printf("Sum of all elements = %d\n",
+         sum2d(rs, cs, varr));
 
   return 0;
->>>>>>> 3314940072979d9cc89f3e3dd92206fee8c6f001
 }
 
 // function with a VLA parameter
 
-int sum2d(int rows, int cols, int ar[rows][cols]) {
+int sum2d(int rows, int cols, int ar[rows][cols])
+{
   int r;
   int c;
   int tot = 0;
