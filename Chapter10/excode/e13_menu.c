31140bc882986c62a8ec0d25136d9f03051ab3c6
klaus <18848113328@163.com>
2022-05-04 11:48:50 +0800

    problem?

---
+++ Chapter10/excode/e13_menu.c
@@ -0,0 +1,32 @@
+/*Write a program that prompts the user to enter three sets of five  double numbers each.
+(You may assume the user responds correctly and doesn’t enter non-numeric data.) The
+program should accomplish all of the following:
+
+a.Store the information in a 3×5 array.
+b.Compute the average of each set of five values.
+c.Compute the average of all the values.
+d.Determine the largest value of the 15 values.
+e.Report the results.
+
+Each major task should be handled by a separate function using the traditional C
+approach to handling arrays. Accomplish task “b” by using a function that computes
+and returns the average of a one-dimensional array; use a loop to call this function three
+times. The other tasks should take the entire array as an argument, and the functions
+performing tasks “c” and “d” should return the answer to the calling program.
+*/
+#include <stdio.h>
+#define ROWS 3
+#define COLS 5
+
+int main(void)
+{
+    double subinput_A[COLS];
+    double subinput_B[COLS];
+    double subinput_B[COLS];
+
+    double input[ROWS][COLS];
+    for (int r = 0; r < ; i++)
+    {
+        /* code */
+    }
+}
