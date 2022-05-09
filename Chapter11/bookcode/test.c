2980aff41b5f87b7626c308cae1d1ca5d0963a09
klaus <18848113328@163.com>
2022-05-09 20:24:02 +0800

    half

--- Chapter11/bookcode/test.c
+++ Chapter11/bookcode/test.c
@@ -7,33 +7,6 @@
 
 int main(void)
 {
-    char flower[SIZE];
-    char addon[] = "123456789 123456789 123456";
-    char bug[BUGSIZE];
-    int available;
-
-    // printf("%ld\n", strlen("liy"));
-    puts("What's your favorite flower?");
-    s_gets(flower, SIZE);
-
-    // printf("%ld\n", strlen(flower));
-    // printf("%ld\n", sizeof(flower));
-
-    if ((strlen(addon) + strlen(flower) + 1) <= SIZE)
-        strcat(flower, addon);
-
-    puts(flower);
-
-    // printf("%ld\n", strlen(flower));
-    // printf("%ld\n", sizeof(flower));
-
-    puts("What is your favorite bug?");
-    s_gets(bug, BUGSIZE);
-
-    available = BUGSIZE - strlen(bug) - 1;
-
-    strncat(bug, addon, available);
-
-    puts(bug);
-    return 0;
+    char *str;
+    strcpy(str, "walkman");
 }
