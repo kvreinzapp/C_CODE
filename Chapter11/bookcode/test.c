bc463b06e6466d966262d06f6a96dcb50fe1dc7e
klaus <18848113328@163.com>
2022-05-10 16:10:40 +0800

    I'm such a loser?

--- Chapter11/bookcode/test.c
+++ Chapter11/bookcode/test.c
@@ -1,39 +1,31 @@
+#include "Chapter11.h"
 #include <stdio.h>
 #include <string.h>
-#include "Chapter11.h"
 
 #define SIZE 30
 #define BUGSIZE 13
 
-int main(void)
-{
-    char *str;
-    strcpy(str, "walkman");
+int main(void) {
+  char *str;
+  strcpy(str, "walkman");
 }
 
-char *s_gets(char *st, int n)
-{
-    int *ret_val;
-    int i = 0;
+char *s_gets(char *st, int n) {
+  int *ret_val;
+  int i = 0;
 
-    ret_val = fgets(st, n, stdin);
-    if (ret_val)
-    {
-        while ((st[i] != '\n') && (st[i] != '\0'))
-        {
-            i++;
-        }
-        if (st[i] == '\n')
-        {
-            st[i] == '\0';
-        }
-        else
-        {
-            while (getchar() != '\n')
-            {
-                continue;
-            }
-        }
+  ret_val = fgets(st, n, stdin);
+  if (ret_val) {
+    while ((st[i] != '\n') && (st[i] != '\0')) {
+      i++;
     }
-    return ret_val;
+    if (st[i] == '\n') {
+      st[i] == '\0';
+    } else {
+      while (getchar() != '\n') {
+        continue;
+      }
+    }
+  }
+  return ret_val;
 }
