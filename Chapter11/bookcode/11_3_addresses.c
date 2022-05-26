fb5891930fffa345722089726b8201bf707189e5
klaus <18848113328@163.com>
2022-05-26 14:29:24 +0800

    nothing

--- Chapter11/bookcode/11_3_addresses.c
+++ Chapter11/bookcode/11_3_addresses.c
@@ -1,14 +1,24 @@
 //  addresses.c  -- addresses of strings
-#define MSG "I'm special."
+// #define MSG "I'm special."
 #include <stdio.h>
 int main()
 {
-    char ar[] = MSG;
-    const char *pt = MSG;
+    // char ar[] = MSG;
+    // const char *pt = MSG;
+    // printf("address of \"I'm special\": %p \n", "I'm special");
+    // printf("          address of MSG: %p\n", MSG);
+    // printf("              address ar: %p\n", ar);
+    // printf("              address pt: %p\n", pt);
+    // printf("          address of MSG: %p\n", MSG);
+    // printf("address of \"I'm special\": %p \n", "I'm special");
+
+    char ar[] = "I'm special.";
+    const char *pt = "I'm special.";
     printf("address of \"I'm special\": %p \n", "I'm special");
+
     printf("              address ar: %p\n", ar);
     printf("              address pt: %p\n", pt);
-    printf("          address of MSG: %p\n", MSG);
+
     printf("address of \"I'm special\": %p \n", "I'm special");
 
     return 0;
--- Chapter11/bookcode/test.c
+++ Chapter11/bookcode/test.c
@@ -1,53 +1,10 @@
-#include "Chapter11.h"
 #include <stdio.h>
-#include <string.h>
+int main(void)
+{
+  char *p1 = "Gcc";
+  p1[0] = 'g';
+  printf("Gcc");
+  printf("%s", "Gcc");
 
-#define SIZE 30
-#define BUGSIZE 13
-
-int main(void) {
-  char *str;
-  strcpy(str, "walkman");
-}
-
-char *s_gets(char *st, int n) {
-  int *ret_val;
-  int i = 0;
-
-<<<<<<< HEAD
-    ret_val = fgets(st, n, stdin);
-
-    if (ret_val)
-    {
-        while ((st[i] != '\n') && (st[i] != '\0'))
-        {
-            i++;
-        }
-        if (st[i] == '\n')
-        {
-            st[i] = '\0';
-        }
-        else
-        {
-            while (getchar() != '\n')
-            {
-                continue;
-            }
-        }
-=======
-  ret_val = fgets(st, n, stdin);
-  if (ret_val) {
-    while ((st[i] != '\n') && (st[i] != '\0')) {
-      i++;
-    }
-    if (st[i] == '\n') {
-      st[i] == '\0';
-    } else {
-      while (getchar() != '\n') {
-        continue;
-      }
->>>>>>> bc463b06e6466d966262d06f6a96dcb50fe1dc7e
-    }
-  }
-  return ret_val;
+  return 0;
 }
