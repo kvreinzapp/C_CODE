a92326b2a6af4506076635a47fc6b47bdd315a01
klaus <18848113328@163.com>
2022-06-02 20:49:22 +0800

    ex2 of Chapter11

--- Chapter11/excode/ex2_modify_ex1.c
+++ Chapter11/excode/ex2_modify_ex1.c
@@ -4,35 +4,65 @@ characters or after the first blank, tab, or newline, whichever comes
 first. (Don’t just use  scanf() .)
 */
 #include <stdio.h>
-char *fetch_char(char ar[], int n);
+char *stop_char(char ar[], int n);
 int main(void)
 {
+    int length;
     char *cat;
     char source[20];
-    cat = fetch_char(source, 10);
-    ` puts(source);
+    printf("How manny characters you wanna input?:");
+    scanf("%d", &length);
+    getchar(); // get rid of the '\n'
+    puts("your input is:");
+    cat = stop_char(source, length);
+    if (cat == NULL)
+        puts("I met,so i stop.\n");
+    else
+    {
+        puts("your output is:");
+        puts(source);
+    }
 
     return 0;
 }
 
-char *fetch_char(char ar[], int n) // just a for //if EOF, break and return NULL，That's all.
+char *stop_char(char ar[], int n) // just a for //if EOF, break and return NULL，That's all.
 {
     int i;
 
     for (i = 0; i < n; i++)
     {
         ar[i] = getchar();
-        if (ar[i] == EOF)
+        if ((ar[i] == EOF) || (ar[i] == ' ') || (ar[i] == '\n') || (ar[i] == '\t'))
             break;
     }
-    if (ar[i] == EOF)
+    if ((ar[i] == EOF) || (ar[i] == ' ') || (ar[i] == '\n') || (ar[i] == '\t'))
     {
+        if (ar[i] == ' ')
+            printf("I met a blank.\n");
+        else if (ar[i] == '\n')
+            printf("I met a newline.\n");
+        else if (ar[i] == '\t')
+            printf("I met a tab.\n");
+        else
+            printf("Invalid inputs.\n");
+
         return NULL;
     }
     else
     {
-        printf("Now i is %d\n", i);
+        printf("Now %d %s is entered.\n", i, i > 1 ? "characters" : "character");
         ar[i] = '\0';
         return ar;
     }
+}
+
+void branch(char sth)
+{
+    if (sth == ' ')
+        printf("I met a blank.\n");
+    else if (sth == '\n')
+        printf("I met a newline.\n");
+    else if (sth == '\t')
+        printf("I met a tab.\n");
 }
