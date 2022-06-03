af88d1b51ba0fc41f4e09666779e4b399ccafb49
klaus <18848113328@163.com>
2022-06-03 21:09:57 +0800

    ex4 of Chapter11

---
+++ Chapter11/excode/ex5_search_string.c
@@ -0,0 +1,16 @@
+
+/*
+Design and test a function that
+1 searches the string specified by the first function parameter for the first occurrence
+    of a character specified by the second function parameter.
+2 Have the function return a pointer to the character if successful, and a null if the
+    character is not found in the string.
+(This duplicates the way that the library strchr()  function works.)
+3 Test the function in a complete program that uses a loop to provide input values for feeding to the function.
+*/
+
+#include <stdio.h>
+char *search_char(char *str, char x);
+int main(void)
+{
+}
