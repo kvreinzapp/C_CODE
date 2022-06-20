c1c773bff05f8c01396cbe381254f28c1cfb19c0
klaus <18848113328@163.com>
2022-06-20 20:43:46 +0800

    Final Chapter, but a little bit concern still here

--- Chapter17.Advanced_Data_Represantation/bookcode/shadow.c
+++ Chapter17.Advanced_Data_Represantation/bookcode/shadow.c
@@ -1,50 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 #define TSIZE 45 /*size of array to hold title*/
-#define FMAX 5   /*maximum number of film titles*/
+// #define FMAX 5   /*maximum number of film titles*/
+#define FMAX 3 /*maximum number of film titles*/
 
-struct film {
-  char title[TSIZE];
-  int rating;
+struct film
+{
+    char title[TSIZE];
+    int rating;
 };
 
 char *s_gets(char *st, int n);
-int main(void) {
-  struct film movies[FMAX];
-  int i = 0;
-  int j;
 
-  puts("Enter first movie title:");
-  while (i < FMAX && s_gets(movies[i].title, TSIZE) != NULL &&
-         movies[i].title[0] != '\0') {
-    puts("Enter your rating <0-10>:");
-    scanf("%d", &movies[i++].rating);
-    while (getchar() != '\n')
-      continue;
-    puts("Enter next tiltle (Empty line to stop):");
-  }
-  if (i == 0)
-    printf("No data entered. ");
-  else
-    printf("Here is your movie list:\n");
+int main(void)
+{
+    struct film movies[FMAX]; // create an array of structure
 
-  for (j = 0; j < i; j++)
-    printf("Movie: %s Rating: %d\n", movies[j].title, movies[j].rating);
-  printf("Bye!\n");
-  return 0;
-}
-char *s_gets(char *st, int n) {
-  char *ret_val;
-  char *find;
+    int i = 0;
+    int j;
 
-  ret_val = fgets(st, n, stdin);
-  if (ret_val) {
-    find = strchr(st, '\n'); // look for newline
-    if (find)                // if the adress id not NULL
-      *find = '\0';          // place a nul character here
+    puts("Enter first movie title:");
+    while (i < FMAX && s_gets(movies[i].title, TSIZE) != NULL &&
+           movies[i].title[0] != '\0')
+    {
+        puts("Enter your rating <0-10>:");
+        scanf("%d", &movies[i++].rating);
+        while (getchar() != '\n')
+            continue;
+        puts("Enter next tiltle (Empty line to stop):");
+    }
+    if (i == 0)
+        printf("No data entered. ");
     else
-      while (getchar() != '\n')
-        continue; // dipose of rest of line
-  }
-  return ret_val;
+        printf("Here is your movie list:\n");
+
+    for (j = 0; j < i; j++)
+        printf("Movie: %s Rating: %d\n", movies[j].title, movies[j].rating);
+    printf("Bye!\n");
+    return 0;
+}
+char *s_gets(char *st, int n)
+{
+    char *ret_val;
+    char *find;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        find = strchr(st, '\n'); // look for newline
+        if (find)                // if the adress id not NULL
+            *find = '\0';        // place a nul character here
+        else
+            while (getchar() != '\n')
+                continue; // dipose of rest of line
+    }
+    return ret_val;
 }
